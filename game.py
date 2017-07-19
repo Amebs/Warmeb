d12f284408b2571632812eee31d6a1e30e6b328d
Amebs <amebs@skripkarev.ru>
2017-07-19 16:23:43 +0400

    add file game.py

---
+++ game.py
@@ -0,0 +1,41 @@
+import pygame
+from pygame import *
+
+width = 800
+height = 640
+display = (width, height)
+bag_color = "#004400"
+black = [0, 0, 0]
+x = 100
+y = 100
+
+
+def test(xy):
+    pygame.draw.line(xy, black, [x - 30, y - 50], [x + 30, y - 50], 5)
+    pygame.draw.line(xy, black, [x + 30, y - 50], [x + 60, y], 5)
+    pygame.draw.line(xy, black, [x + 60, y], [x + 30, y + 50], 5)
+    pygame.draw.line(xy, black, [x + 30, y + 50], [x - 30, y + 50], 5)
+    pygame.draw.line(xy, black, [x - 30, y + 50], [x - 60, y], 5)
+    pygame.draw.line(xy, black, [x - 60, y], [x - 30, y - 50], 5)
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(display)
+    pygame.display.set_caption("Warmeb")
+    bg = Surface((width, height))
+
+    bg.fill(Color(bag_color))
+
+    while 1:
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                raise SystemExit, "QUIT"
+        screen.blit(bg, (0, 0))
+
+        test(screen)
+
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main()
