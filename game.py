1ed68dcea932fac7601196f74cc7d6862b52bca8
Amebs <amebs@skripkarev.ru>
2017-08-01 18:57:44 +0400

    assign

--- game.py
+++ game.py
@@ -6,60 +6,122 @@ width = 800
 height = 640
 display = (width, height)
 bag_color = "#004400"
-black = [0, 0, 0]
-radius = 30
-fat = 2
+hex_color_1 = [200, 200, 200, 200]
+hex_color_2 = [10, 30, 70, 100]
+black = [200, 200, 200]
+radius = 10
+fat = 1
 point = [0, 0]
+point_new = [0, 0]
+point_new[0] = 0#4*radius * 3 / 2
+point_new[1] = 0#4*2 * (math.sqrt(3) / 2 * radius)
+startxy = [0, 0]
 
 
-def draw_hexapod(screen, point):
+def draw_hexapod(screen, point, radius):
     hexagon = []
     for i in range(0, 6):
         hexagon.append(hex_corner(point[0], point[1], radius, i))
     for i in range(0, 5):
-            pygame.draw.line(screen, black, hexagon[i], hexagon[i+1], fat)
+        pygame.draw.line(screen, black, hexagon[i], hexagon[i+1], fat)
     pygame.draw.line(screen, black, hexagon[5], hexagon[0], fat)
 
 
+def draw_hexagon(screen, point, radius, hex_color):
+   hexagon = []
+   for i in range(0, 6):
+       hexagon.append(hex_corner(point[0], point[1], radius, i))
+   for i in range(0, 5):
+       pygame.draw.polygon(screen, hex_color, hexagon, 1)
+
+
 def hex_corner(center_x, center_y, size, i):
-    point_xy = [0, 0]
-    angle_deg = 60 * i
-    angle_rad = math.pi / 180 * angle_deg
-    point_xy[0] = center_x + size * math.cos(angle_rad)
-    point_xy[1] = center_y + size * math.sin(angle_rad)
-    return point_xy
+   point_xy = [0, 0]
+   angle_deg = 60 * i
+   angle_rad = math.pi / 180 * angle_deg
+   point_xy[0] = center_x + size * math.cos(angle_rad)
+   point_xy[1] = center_y + size * math.sin(angle_rad)
+   return point_xy
+
+def field(screen, point, radius, hex_color):
+   for n in range(0, 50):
+       for i in range(0, 50):
+           if (i % 2):
+               draw_hexagon(screen, point, radius, hex_color)
+               point[0] = i * radius * 3 / 2
+               point[1] = math.sqrt(3) / 2 * radius + n * 2 * (math.sqrt(3) / 2 * radius)
+           else:
+               draw_hexagon(screen, point, radius, hex_color)
+               point[0] = i * radius * 3 / 2
+               point[1] = n * 2 * (math.sqrt(3) / 2 * radius)
 
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode(display)
-    pygame.display.set_caption("Warmeb")
-    bg = Surface((width, height))
-    bg.fill(Color(bag_color))
+   pygame.init()
+   screen = pygame.display.set_mode(display)
+   pygame.display.set_caption("Warmeb")
+   bg = Surface((width, height))
+   bg.fill(Color(bag_color))
 
-    while 1:
-        for e in pygame.event.get():
-            if e.type == QUIT:
-                raise SystemExit, "QUIT"
-        screen.blit(bg, (0, 0))
+   while 1:
+       for e in pygame.event.get():
+           if e.type == QUIT:
+               raise SystemExit, "QUIT"
+
+           if e.type == pygame.KEYDOWN:
+
+               global point_new
+               global radius
+               global fat
+
+               if e.key == pygame.K_END:
+                   bg.fill(Color("#55ffff"))
+
+               if e.key == pygame.K_PAGEDOWN:
+                   point_new[0] += 3*radius/2
+                   point_new[1] += (math.sqrt(3) / 2 * radius)
+
+               if e.key == pygame.K_DELETE:
+                   point_new[0] -= 3*radius/2
+                   point_new[1] += (math.sqrt(3) / 2 * radius)
+
+               if e.key == pygame.K_DOWN:
+                   point_new[1] += 2 * (math.sqrt(3) / 2 * radius)
+
+               if e.key == pygame.K_UP:
+                   point_new[1] -= 2 * (math.sqrt(3) / 2 * radius)
 
 
-        for n in range(0, 50):
-            for i in range(0,50):
-                if (i%2):
-                    draw_hexapod(screen, point)
-                    point[0] = i * radius * 3 / 2
-                    point[1] = math.sqrt(3) / 2 * radius+n*2*(math.sqrt(3) / 2 * radius)
-                else:
-                    draw_hexapod(screen, point)
-                    point[0] = i * radius * 3 / 2
-                    point[1] = n*2*(math.sqrt(3) / 2 * radius)
+               if e.key == pygame.K_RIGHT:
+                   point_new[0] += 3*radius
 
+               if e.key == pygame.K_LEFT:
+                   point_new[0] -= 3*radius
 
-        point[0] = 0 #  x
-        point[1] = 0 #  y
-        pygame.display.update()
+           if e.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:
+               #left mouse button
+               global radius
+               radius=radius+10
+
+               break
+           if e.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[2]:
+               #right mouse button
+               global radius
+               if radius>10:
+                   radius = radius-10
+
+               #field(screen, point, radius)
+               #startxy[0] += 50
+               #startxy[1] += 50
+               break
+       screen.blit(bg, startxy)
+       field(screen, point, radius, hex_color_2)
+       fat = 3
+       draw_hexapod(screen, point_new, radius)
+       point[0] = 0 # x
+       point[1] = 0 # y
+       pygame.display.update()
 
 
 if __name__ == "__main__":
-    main()
+   main()
